ebd1e2a1f24ab3732f37a5ccc90bf22c465ce853
pandapm <panda_m@126.com>
2016-05-04 21:24:29 +0800

    getTags

--- modules/article/getTag.js
+++ modules/article/getTag.js
@@ -4,33 +4,44 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-function  getTags (slug){
+function  getTags (slug ,callback){
     if(slug == undefined || slug == null){
-        //callback('Param Error');
+        callback('Param Error');
         return;
     }
     var slugs=[,'marry','family','career','healthy','science'];
     if(slug !== '' && slugs.indexOf(slug)<0){
-        //callback('Param Error');
+        callback('Param Error');
         return;
     }
     var url = 'http://www.xinli001.com/info/';
+    var i = slugs.indexOf(slug);
+    if(i === -1){
+        i=0;
+    }
+    //console.log('i='+i);
     request(
         {
            uri: url
         },function (err,res,body){
             if(err){
-                //callback(err);
+                callback(err);
                 return;
             }
-            console.log(url);
-            var $ = cheerio.load(body,{decodeEntities:false});
-            //console.log($('.selector-tag').html());
-            var uls =$('.selector-tag');// cheerio.load($('.selector-tag').html(),{decodeEntities:false});
-            console.log(uls);
-            uls.forEach(function (ul){
+             var $ = cheerio.load(body,{decodeEntities:false,ignoreSpace:true});
+             var uls =$('.selector-tag');
+            var tags=[];
+            uls[i].children.forEach(function (it){
+                    if(it.name == 'li'){
+                        //console.log();
+                         tags.push(it.attribs['data-tag'])
+                    }
+                });
+            var resu = {
+                tags:tags
+            }
+            callback(resu)
 
-            })
 
         }
 
@@ -39,4 +50,5 @@ function  getTags (slug){
 
 }
 
-//getTags("");
+//getTags("science");
+module.exports = getTags ;
--- routes/article.js
+++ routes/article.js
@@ -4,10 +4,12 @@
 var express = require('express');
 var router = express.Router();
 var parsers = require('../modules/parsers');
+
 var getList = require('../modules/article/getList');
 var getDetail = require('../modules/article/getDetail');
+var getTags = require('../modules/article/getTag');
 router.use('/getList',function(req,res){
-    //console.log(req.query)
+
     var s = (req.query.sort== null) ? "":req.query.sort;
     var f = (req.query.flag == null) ? "":req.query.flag;
     var p = (req.query.page == null) ? 1:req.query.page;
@@ -26,6 +28,12 @@ router.use('/getList',function(req,res){
 router.use('/getDetail/:id',function(req,res){
     var id = req.params.id;
     getDetail(id,function(result){
+        parsers.resultProc(req,result,res);
+    })
+});
+router.use('/getTags',function (req,res){
+    var slug = (req.query.sort == null)?"":req.query.sort;
+    getTags(slug,function(result){
         parsers.resultProc(req,result,res);
     })
 })
